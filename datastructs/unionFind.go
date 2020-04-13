3f1190f9a40210bef6be319b56c08ac0886e4983
Mikhail Kharchenko <kharchenkoms@gmail.com>
2020-04-13 18:38:35 +0300

    add union find data structure

---
+++ datastructs/unionFind.go
@@ -0,0 +1,56 @@
+package datastructs
+
+// UnionFind datastructure interface
+type UnionFind interface {
+	Union(s1, s2 int)
+	Find(val int) int
+	Connected(v1, v2 int) bool
+}
+
+type disjointSet struct {
+	elements []int
+	size     []int
+}
+
+// InitUnionFind creates union find datastruct from the given number [1..nums]
+func InitUnionFind(nums int) UnionFind {
+	ds := disjointSet{elements: make([]int, nums+1), size: make([]int, nums+1)}
+	for i := 1; i <= nums; i++ {
+		ds.elements[i] = i
+		ds.size[i] = 1
+	}
+	return &ds
+}
+
+// UnionFindFromSlice creates union find datastruct from slice
+func UnionFindFromSlice(slice []int) UnionFind {
+	ds := disjointSet{elements: make([]int, len(slice)), size: make([]int, len(slice))}
+	for i := 0; i < len(slice); i++ {
+		ds.elements[i] = slice[i]
+		ds.elements[i] = 1
+	}
+	return &ds
+}
+
+func (s *disjointSet) Union(s1, s2 int) {
+	s1, s2 = s.Find(s1), s.Find(s2)
+	if s1 == s2 {
+		return
+	}
+	if s.size[s1] > s.size[s2] {
+		s1, s2 = s2, s1
+	}
+	s.elements[s1] = s2
+	s.size[s2] += s.size[s1]
+}
+
+func (s *disjointSet) Find(val int) int {
+	if val != s.elements[val] {
+		s.elements[val] = s.Find(s.elements[val])
+	}
+	return s.elements[val]
+}
+
+func (s *disjointSet) Connected(v1, v2 int) bool {
+	return s.Find(v1) == s.Find(v2)
+}
